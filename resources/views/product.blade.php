95cc812c687e6d3aa3199446e7f5637b1730c629
Valentina2668 <68551592+Valentina2668@users.noreply.github.com>
2024-03-27 15:06:10 +0300

    laravel-project

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -9,16 +9,12 @@
     <form action="{{asset('add_cart/'.$product->id)}}">
         <div class="container px-5 pt-1 mx-auto">
             <div class="product-one">
-                <div class="image">
+                <div class="w-[400px] m-auto md:flex-1">
                     <img src="/storage/{{$product->picture}}" alt="ecommerce">
                 </div>
-                <div class="content">
+                <div class="content md:flex-1">
                     <h1>{{$product->name}}</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam laudantium
-                        rem ea nostrum suscipit, iure excepturi inventore consectetur provident tempore magnam molestiae
-                        obcaecati eveniet ex dolores labore deserunt cumque eum.</p>
-
-                    <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+                    <p>{!!$product->body!!}</p>
                     <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                         <div class="flex ml-6 items-center">
                             <span class="mr-3">Размер</span>
@@ -38,7 +34,7 @@
                     </div>
                     <div class="flex justify-center items-center">
                         <span class="title-font font-medium text-3xl text-green-600">{{$product->discount}} <span> BYN</span> </span> <span class="title-font font-medium text-xl ml-8">{{$product->price}} <span> BYN</span> </span>
-                        <button type="submit" class="flex ml-auto text-white font-medium bg-green-500 border-0 py-6 px-12 focus:outline-none hover:bg-green-600 rounded">В корзину</button>
+                        <button type="submit" class="flex ml-auto text-white font-medium bg-green-500 border-0 m-5 py-4 px-12 focus:outline-none hover:bg-green-600 rounded">В корзину</button>
                         @if ($favorite) <a href="{{asset('product/' .$favorite->product->id.'/remove_from_favorites')}}" class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-red-500 ml-4">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -31,16 +31,16 @@
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
+                        {{ __('Ваш email не верифицирован') }}
 
                         <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
+                            {{ __('Нажмите сюда, чтобы отправить еще раз подтверждение на email.') }}
                         </button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
+                            {{ __('Новая ссылка на верификацию была отправлениа на Ваш email.') }}
                         </p>
                     @endif
                 </div>
@@ -48,7 +48,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button>{{ __('Сохранить') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
@@ -57,7 +57,7 @@
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
                     class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
+                >{{ __('Сохранить') }}</p>
             @endif
         </div>
     </form>
